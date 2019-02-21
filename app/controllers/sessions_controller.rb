4b38df270f9b7c0879d9d609701c32e57b700e40
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-21 22:25:14 +0000

    Define create & destroy methods in SessionsC. Define new & create methods, and user_params private method, in UsersC. Fix typo in new sessions view. Rewrite form path to be more railsy (new users), and add error message in view. Delete CreateUsers migration file
    which I should not have done. I had messed up my initial migration and then further messed things up while trying to fix it. Thanks, Vaz, for saving my butt.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/new_session_path'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/new_session_path'
   end
 end
