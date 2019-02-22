f8c394c733f0a749ec95d625a6a6db804ea9591f
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-22 03:02:49 +0000

    Add method to UserMailer class.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'no-reply@jungle.com'
+
+  def thank_you_email(user)
+    @user = user
+    @url = 'http://example.com'
+    mail(to: @user.email, subject: 'Thanks for your order!')
+  end
 end
