4864499a51ac4da2d841e330532b705c194df2ea
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-22 03:48:57 +0000

    Write email, populate with order contents.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,8 @@
 class UserMailer < ApplicationMailer
   default from: 'no-reply@jungle.com'
 
-  def thank_you_email(user)
-    @user = user
-    @url = 'http://example.com'
-    mail(to: @user.email, subject: 'Thanks for your order!')
+  def thank_you_email(order)
+    @order = order
+    mail(to: @order.email, subject: 'Thanks for your order!')
   end
 end
