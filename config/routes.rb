803c1bffe5823e567bf22223347e2327201d1913
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-23 06:44:40 +0000

    Add destroy route to reviews.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#logout'
 
   resources :products, only: [:index, :show] do
-    resources :reviews, only: [:create]
+    resources :reviews, only: [:create, :destroy]
   end
   resources :categories, only: [:show]
 
