848784ae1bfd6b6b9d9bacd58d545ff76cb5f35a
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-25 20:43:05 +0000

    First test - valid with valid attrs - runs successfully.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+
   describe 'Validations' do
-    it 'is valid with valid attributes'
+    it 'is valid with valid attributes' do
+      @category = Category.create
+      @product = Product.new(
+        name: 'a name',
+        price: 123,
+        quantity: 123,
+        category_id: @category.id
+        )
+      expect(@product).to be_valid
+    end
+
     it 'is not valid without a name'
     it 'is not valid without a price'
     it 'is not valid without a quantity'
