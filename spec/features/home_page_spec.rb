059c499b45670c0515713b312fac2034120409fa
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-26 19:47:09 +0000

    Remove placeholder code in home_page_spec; add js: true option.

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
-RSpec.feature "HomePages", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "HomePages", type: :feature, js: true do
 end
