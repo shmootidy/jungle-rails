5edecda6cdebb1b8944d34ca620682a9f5f6515a
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-26 21:05:52 +0000

    Describe scenario for user_login spec. Add 4-step comments and screenshot save line.

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "UserLogins", type: :feature, js: true do
+  #SETUP
   before :each do
     @user = User.create!(
       first_name: 'Wonder',
@@ -9,5 +10,11 @@ RSpec.feature "UserLogins", type: :feature, js: true do
       password: 'password123',
       password_confirmation: 'password123'
       )
+  end
+  scenario 'A registered user can log in using the log in form' do
+    #ACT
+    #DEBUG
+    save_screenshot 'user-login.jpg'
+    #VERIFY
   end
 end
