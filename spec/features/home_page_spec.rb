8a4e4a099cd9338dbb08ec9b8e10679f7844a776
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-26 19:48:13 +0000

    Describe first testing scenario.

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "HomePages", type: :feature, js: true do
+
+  scenario 'They see all products' do
+  # ACT
+  visit root_path
+
+  # DEBUG / VERIFY
+  save_screenshot
+  end
+
 end
