c7b11f0e3138125bf57521e06de0caf254d9706b
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-26 20:13:34 +0000

    Add before :each block to generate new category and products within that category.

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,14 +2,28 @@ require 'rails_helper'
 
 RSpec.feature "Visitor navigates to the home page", type: :feature, js: true do
 
+  before :each do
+    @category = Category.create! near: 'Apparel'
+
+    10.time do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(1),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
   scenario 'They see all products' do
-  # ACT
-  visit root_path
+    # ACT
+    visit root_path
 
-  # DEBUG / VERIFY
-  save_screenshot('screenshot.png')
+    # DEBUG
+    save_screenshot('screenshot.png')
 
-  expect(page).to have_css 'article.product'
+    #VERIFY
+    expect(page).to have_css 'article.product'
   end
 
 end
