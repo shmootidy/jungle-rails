b9fbf1782ec609040b468f824f6b98dab16034ae
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-26 20:16:08 +0000

    Edit typos. Test now runs GREEN.

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.feature "Visitor navigates to the home page", type: :feature, js: true do
 
   before :each do
-    @category = Category.create! near: 'Apparel'
+    @category = Category.create! name: 'Apparel'
 
-    10.time do |n|
+    10.times do |n|
       @category.products.create!(
         name: Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(1),
