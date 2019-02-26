425b989aa6f9dde13e87e7f4b89a66c921fed623
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-26 20:24:43 +0000

    Add the same before :each block from home_page_spec to product_detail_spec. Edit the feature line to include JS in the same way too
    though, admittedly, I'm not 100% certain this is required. Makes sense that it would be.

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(1),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
 end
