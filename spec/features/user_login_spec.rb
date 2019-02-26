d2db10c78d25e6952945cb3d9909bce3172663c0
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-26 21:04:10 +0000

    Add js: true to user_login spec. Add User.create! with details, in before :each block.

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.feature "UserLogins", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "UserLogins", type: :feature, js: true do
+  before :each do
+    @user = User.create!(
+      first_name: 'Wonder',
+      last_name: 'Woman',
+      email: 'wonder@woman.com',
+      password: 'password123',
+      password_confirmation: 'password123'
+      )
+  end
 end
