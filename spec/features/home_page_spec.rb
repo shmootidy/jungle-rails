2463fecfdeaf4f46c0202afc7d589521e738e7c5
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-26 20:11:16 +0000

    Edit title of RSpec.feature. Add expect line. RED.

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "HomePages", type: :feature, js: true do
+RSpec.feature "Visitor navigates to the home page", type: :feature, js: true do
 
   scenario 'They see all products' do
   # ACT
@@ -8,6 +8,8 @@ RSpec.feature "HomePages", type: :feature, js: true do
 
   # DEBUG / VERIFY
   save_screenshot('screenshot.png')
+
+  expect(page).to have_css 'article.product'
   end
 
 end
